658122aca8f2a74d3d8997ad570992dda6c5b9be
Shirleys-os <>
2025-10-12 13:16:17 +0000

    Update from https://github.com/Shirleys-os/backend/commit/d2308652ec44f56f6a8073591e0b1ac5d013be3f

--- src/lib/api/customer/client/models/AdminGetOrderByIdResponse.ts
+++ src/lib/api/customer/client/models/AdminGetOrderByIdResponse.ts
@@ -3,6 +3,24 @@
 /* tslint:disable */
 /* eslint-disable */
 export type AdminGetOrderByIdResponse = {
+  orderInfo: {
+    billingCountry: string | null;
+    billingZipCode: string | null;
+    billingProvince: string | null;
+    billingCity: string | null;
+    billingAddress2: string | null;
+    billingAddress1: string | null;
+    shippingCountry: string | null;
+    shippingZipCode: string | null;
+    shippingProvince: string | null;
+    shippingCity: string | null;
+    shippingAddress2: string | null;
+    shippingAddress1: string | null;
+    note: string | null;
+    email: string | null;
+    phone: string | null;
+    name: string | null;
+  } | null;
   orderItems: Array<{
     total: number;
     quantity: number;
