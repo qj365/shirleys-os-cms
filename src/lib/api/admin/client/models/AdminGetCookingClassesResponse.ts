e057016c22e079521d5c01634b5c64fc2215a492
Shirleys-os <>
2025-10-22 15:04:15 +0000

    Update from https://github.com/Shirleys-os/backend/commit/7b2689dfe2bf1a410fecea73cbf688eb970ac03d

--- src/lib/api/admin/client/models/AdminGetCookingClassesResponse.ts
+++ src/lib/api/admin/client/models/AdminGetCookingClassesResponse.ts
@@ -7,6 +7,7 @@ export type AdminGetCookingClassesResponse = {
     availableSlots: number;
     maxSlots: number;
     dateTime: string;
+    id: number;
   }>;
   address: string;
   price: number;
